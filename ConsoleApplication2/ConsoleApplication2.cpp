78a86e55ee0f903cf501945d8766ad6aa01877e7
RianovRV1 <potaco2@hotmail.com>
2014-12-05 13:11:43 -0800

    Map Generator with walls
    Has walls, doors, and a limit to how many walls can spawn inside the
    room

--- ConsoleApplication2/ConsoleApplication2.cpp
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -9,14 +9,14 @@
 #include <stdlib.h>
 #include <sstream>
 using namespace std;
-
+const int TOTAL_TILE_SPRITES = 4; 
 string FillNumber(int num);
-void WriteToMap();
+void WriteToMap(int height, int width);
 int _tmain(int argc, _TCHAR* argv[])
 {
 	srand(time(NULL));
 	string line;
-	WriteToMap();
+	WriteToMap(10, 16);
 	ifstream file2("map.map");
 	if (file2.is_open())
 	{
@@ -26,6 +26,7 @@ int _tmain(int argc, _TCHAR* argv[])
 		}
 		file2.close();
 	}
+	remove("map.map");
 	system("PAUSE");
 	return 0;
 }
@@ -37,16 +38,43 @@ string FillNumber(int num)
 	return fill.str();
 }
 
-void WriteToMap()
+void WriteToMap(int height, int width)
 {
+	int wallLimit = (((2 * (height - 2)) + (2 * width)) / 4);
 	ofstream file("map.map");
-	for (int i = 0; i < 12; ++i)
+	for (int i = 0; i < height; ++i)
 	{
-
-		for (int j = 0; j < 16; ++j)
+		int toLimit = 0;
+		for (int j = 0; j < width; ++j)
 		{
-			int num = rand() % 12;
-			file << FillNumber(num) << " ";
+			
+			if ((i == (height / 2) && (j == 0 || j == (width - 1))) || (j == (width / 2) && (i == 0 || i == (height - 1))))
+			{
+				file << FillNumber(1) << " ";
+			}
+			else if (i == 0 || i == (height - 1) || j == 0 || j == (width - 1) && (i != (height / 2) || j != (width / 2)))
+			{
+				file << FillNumber(0) << " ";
+			}
+
+			else
+			{
+				int num = rand() % TOTAL_TILE_SPRITES;
+				if (num == 0 && toLimit != wallLimit)
+				{
+					file << FillNumber(num) << " ";
+					++toLimit;
+				}
+				else if (num == 0 && toLimit == wallLimit)
+				{
+					num = (rand() % (TOTAL_TILE_SPRITES - 1)) + 1;
+					file << FillNumber(num) << " ";
+				}
+				else
+				{
+					file << FillNumber(num) << " ";
+				}
+			}
 		}
 		file << "\n";
 	}
